0bb1e0fb611efbd43c330aed3775db58dabf95d4
Rohan-Shetty98 <67802017+Rohan-Shetty98@users.noreply.github.com>
2020-08-10 21:43:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('.navbar-toggler').click(function () {
+        $('.navbar-toggler').toggleClass('change');
+    })
+
+    $(window).scroll(function () {
+        let position = $(this).scrollTop();
+
+        if(position >= 196){
+            $('.navbar').addClass('navbar-background');
+            $('.navbar').addClass('fixed-top');
+        }
+        else{
+            $('.navbar').removeClass('navbar-background');
+            $('.navbar').removeClass('fixed-top');
+        }
+    })
+    //smooth scroll
+$('.nav-item a').click(function (link) {
+link.preventDefault();
+let target = $(this).attr('href');
+$('html, body').stop().animate({
+    scrollTop: $(target).offset().top
+}, 3000)
+
+})
+
+
+
+
+
+
+
+
+
+
+});
